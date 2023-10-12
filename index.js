2431228b5d200578fc1dff0c822015de2bacd14b
sahlminkok <sahlminkok7@gmail.com>
2023-10-12 19:44:08 +0100

    Add eslint lint files

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 const projectsData = [
   {
-    imgSrc: "./assets/images/placeholder.jpg",
-    imgAlt: "placeholder",
-    heading: "Multi-Post Stories",
+    imgSrc: './assets/images/placeholder.jpg',
+    imgAlt: 'placeholder',
+    heading: 'Multi-Post Stories',
     paragraph:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    stack: ["Css", "Html", "Bootstrap", "Ruby"],
-    link: "",
+    stack: ['Css', 'Html', 'Bootstrap', 'Ruby'],
+    link: '',
   },
   {
-    imgSrc: "./assets/images/placeholder.jpg",
-    imgAlt: "placeholder",
-    heading: "Multi-Post Stories",
+    imgSrc: './assets/images/placeholder.jpg',
+    imgAlt: 'placeholder',
+    heading: 'Multi-Post Stories',
     paragraph:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    stack: ["Css", "Html", "Bootstrap", "Ruby"],
-    link: "",
+    stack: ['Css', 'Html', 'Bootstrap', 'Ruby'],
+    link: '',
   },
   {
-    imgSrc: "./assets/images/placeholder.jpg",
-    imgAlt: "placeholder",
-    heading: "Multi-Post Stories",
+    imgSrc: './assets/images/placeholder.jpg',
+    imgAlt: 'placeholder',
+    heading: 'Multi-Post Stories',
     paragraph:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    stack: ["Css", "Html", "Bootstrap", "Ruby"],
-    link: "",
+    stack: ['Css', 'Html', 'Bootstrap', 'Ruby'],
+    link: '',
   },
   {
-    imgSrc: "./assets/images/placeholder.jpg",
-    imgAlt: "placeholder",
-    heading: "Multi-Post Stories",
+    imgSrc: './assets/images/placeholder.jpg',
+    imgAlt: 'placeholder',
+    heading: 'Multi-Post Stories',
     paragraph:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    stack: ["Css", "Html", "Bootstrap", "Ruby"],
-    link: "",
+    stack: ['Css', 'Html', 'Bootstrap', 'Ruby'],
+    link: '',
   },
 ];
 
@@ -42,22 +42,22 @@ const projects = document.querySelector('.projects');
 projectsData.forEach((projectData) => {
   const project = document.createElement('div');
   project.className = 'project';
-  
+
   const img = document.createElement('img');
   img.src = projectData.imgSrc;
   img.alt = projectData.imgAlt;
   project.appendChild(img);
 
-  const project_details = document.createElement('div');
-  project_details.className = 'project-details';
+  const projectDetails = document.createElement('div');
+  projectDetails.className = 'project-details';
 
   const h3 = document.createElement('h3');
   h3.textContent = projectData.heading;
-  project_details.appendChild(h3);
+  projectDetails.appendChild(h3);
 
   const p = document.createElement('p');
   p.textContent = projectData.paragraph;
-  project_details.appendChild(p);
+  projectDetails.appendChild(p);
 
   const stack = document.createElement('div');
   stack.className = 'stack';
@@ -68,14 +68,14 @@ projectsData.forEach((projectData) => {
     ul.appendChild(li);
   });
   stack.appendChild(ul);
-  project_details.appendChild(stack);
+  projectDetails.appendChild(stack);
 
   const a = document.createElement('a');
   a.className = 'btn';
   a.href = projectData.link;
   a.textContent = 'See Project';
-  project_details.appendChild(a);
+  projectDetails.appendChild(a);
 
-  project.appendChild(project_details);
+  project.appendChild(projectDetails);
   projects.appendChild(project);
 });
